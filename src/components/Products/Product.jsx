e9675f4c6ae555dfa581d753a6d8278acb65e192
satish chakma <satishchakma.gd@gmail.com>
2023-03-27 00:53:13 +0600

    added styles

--- src/components/Products/Product.jsx
+++ src/components/Products/Product.jsx
@@ -4,12 +4,18 @@ import "./Product.css";
 const Product = (props) => {
   const { id, name, img, price, seller, ratings } = props.product;
   return (
-    <div>
-      <img src={img} alt={name} />
-      <h4> {name}</h4>
-      <h4> Price: ${price}</h4>
-      <p> Manufacturer: {seller}</p>
-      <p> Rating: {ratings}</p>
+    <div className="product">
+      <div>
+        <img src={img} alt={name} />
+        <h4> {name}</h4>
+        <h4> Price: ${price}</h4>
+      </div>
+      <div>
+        <p> Manufacturer: {seller}</p>
+        <p> Rating: {ratings}</p>
+      </div>
+
+      <button onClick={() => props.addToCart(id)}>Add to Cart</button>
     </div>
   );
 };
